fd3f0a10f00bc631768064c62b11ba0c57c37861
Warley Coutinho <warleycoutinho@icloud.com>
2025-09-27 16:16:04 -0300

    terminando a tela home

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:conversor_moedas/app/components/currency_box.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,7 +8,28 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: const Center(child: Text('Welcome to the Home Page!')),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100, left: 30, right: 30, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset('assets/images/logo.png', width: 150, height: 150),
+              SizedBox(height: 80),
+              CurrencyBox(),
+              SizedBox(height: 10),
+              CurrencyBox(),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.amber),
+                child: Text('CONVERTER'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

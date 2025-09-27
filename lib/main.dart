c839af55a91eb566b8909ce5dc9ed7d2f221390c
Warley Coutinho <warleycoutinho@icloud.com>
2025-09-27 09:16:31 -0300

    criando a homePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:conversor_moedas/views/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My First App')),
-        body: Center(child: Text('Hello, World!')),
-      ),
-    );
+    return MaterialApp(theme: ThemeData.dark(), home: HomePage());
   }
 }

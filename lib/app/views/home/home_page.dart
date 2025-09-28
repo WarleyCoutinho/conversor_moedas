78e109d7c84bfbf56cf20f67eb3ef036caec1bbe
Warley Coutinho <warleycoutinho@icloud.com>
2025-09-28 17:00:14 -0300

    add o SingleChildScrollView

--- lib/app/views/home/home_page.dart
+++ lib/app/views/home/home_page.dart
@@ -33,46 +33,48 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page'), centerTitle: true),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 100, left: 30, right: 30, bottom: 20),
-          child: Column(
-            children: [
-              Image.asset('assets/images/logo.png', width: 150, height: 150),
-              SizedBox(height: 80),
-              CurrencyBox(
-                selectedItem: homeController.toCurrency,
-                items: homeController.currencies,
-                controller: toText,
-                onChanged: (model) {
-                  homeController.toCurrency = model;
-                },
-              ),
-              SizedBox(height: 10),
-              CurrencyBox(
-                selectedItem: homeController.fromCurrency,
-                items: homeController.currencies,
-                controller: fromText,
-                onChanged: (model) {
-                  homeController.fromCurrency = model;
-                },
-              ),
-              SizedBox(height: 80),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    homeController.setToCurrency();
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  minimumSize: Size(MediaQuery.of(context).size.width * 0.5, 50),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 100, left: 30, right: 30, bottom: 20),
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png', width: 150, height: 150),
+                SizedBox(height: 80),
+                CurrencyBox(
+                  selectedItem: homeController.toCurrency,
+                  items: homeController.currencies,
+                  controller: toText,
+                  onChanged: (model) {
+                    homeController.toCurrency = model;
+                  },
                 ),
-                child: Text('CONVERTER', style: TextStyle(fontSize: 18, color: Colors.white)),
-              ),
-            ],
+                SizedBox(height: 10),
+                CurrencyBox(
+                  selectedItem: homeController.fromCurrency,
+                  items: homeController.currencies,
+                  controller: fromText,
+                  onChanged: (model) {
+                    homeController.fromCurrency = model;
+                  },
+                ),
+                SizedBox(height: 80),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      homeController.setToCurrency();
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    minimumSize: Size(MediaQuery.of(context).size.width * 0.5, 50),
+                  ),
+                  child: Text('CONVERTER', style: TextStyle(fontSize: 18, color: Colors.white)),
+                ),
+              ],
+            ),
           ),
         ),
       ),
